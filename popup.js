89c85eed6fefb6b6fa91754bbb83e3b56618e7d9
gigabyteZ390 <raphael.varichon-dupre@epitech.eu>
2025-08-05 18:32:49 +0200

    fix: swap the positions of the two buttons / feat: have (i think) put the most revelant format, we can select one of them.

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
-console.log("caca");
+document.addEventListener("DOMContentLoaded", () => {
+  const options = document.querySelectorAll(".dropdown-option");
+
+  options.forEach(option => {
+    option.addEventListener("click", () => {
+      options.forEach(opt => opt.classList.remove("selected"));
+      option.classList.add("selected");
+    });
+  });
+});
+
+
+/*const selectedText = option.cloneNode(true);
+selectedText.querySelector(".indicator")?.remove();
+console.log(selectedText.textContent.trim()); */
+
+
+
+
+
